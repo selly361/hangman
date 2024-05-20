0cfadf5f8608a95b6ec34e06edb495be28391890
selly361 <seikou615@gmail.com>
2024-05-20 18:55:21 +0100

    Update: feat: Implement home page component with logo, play button link, and guide link

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { LogoIcon } from 'Assets/Icons'
+import iconImage from './play-icon.png'
+import { Link } from 'react-router-dom'
 
 function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+	return (
+		<main className='main-menu'>
+			<LogoIcon className='main-menu__logo' />
+			<Link className='button main-menu__play-button' to='/select-category'>
+					<img
+						className='main-menu__play-button__img'
+						src={iconImage}
+						alt='play icon image'
+					/>
+			</Link>
+			<Link className='button--primary' to='guide'>How To Play</Link>
+		</main>
+	)
 }
 
 export default HomePage
