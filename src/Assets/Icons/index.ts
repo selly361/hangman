57fb45f67643458cef90b69c41944ae4b670ba95
selly361 <seikou615@gmail.com>
2024-05-20 18:25:03 +0100

    Add: `index.ts` to export icons

---
+++ src/Assets/Icons/index.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as MenuIcon } from './icon-menu.svg'
+export { ReactComponent as LogoIcon } from './icon-logo.svg'
+export { ReactComponent as HeartIcon } from './icon-heart.svg'
+export { ReactComponent as BackIcon } from './icon-back.svg'
