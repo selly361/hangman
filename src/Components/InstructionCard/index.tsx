dc49c1221b70c4617fd0bd30c68e10aef7976d82
selly361 <seikou615@gmail.com>
2024-05-21 20:15:30 +0100

    Add: `InstructionCard` component

---
+++ src/Components/InstructionCard/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { IInstructionCardProps } from './InstructionCard.types'
+
+function InstructionCard({ index, title, description }: IInstructionCardProps) {
+
+
+  return (
+    <div className='instruction-card'>
+        <h2 className='instruction-card__title'>0{index + 1}</h2>
+        <h3 className='instruction-card__sub-title'>{title}</h3>
+        <p className='instruction-card__description'>{description}</p>
+    </div>
+  )
+}
+
+export default InstructionCard
