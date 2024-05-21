2549af79238de8e026ab42311f8a65550ac599d9
selly361 <seikou615@gmail.com>
2024-05-21 20:32:16 +0100

    Fix: add `key` prop

--- src/Components/InstructionCard/index.tsx
+++ src/Components/InstructionCard/index.tsx
@@ -5,7 +5,7 @@ function InstructionCard({ index, title, description }: IInstructionCardProps) {
 
 
   return (
-    <div className='instruction-card'>
+    <div className='instruction-card' key={title}>
         <h2 className='instruction-card__title'>0{index + 1}</h2>
         <h3 className='instruction-card__sub-title'>{title}</h3>
         <p className='instruction-card__description'>{description}</p>
