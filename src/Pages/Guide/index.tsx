2f0c02bbb253f9ce643751e557f44fb2135b04c6
selly361 <seikou615@gmail.com>
2024-05-25 18:35:29 +0100

    Update: refactor `Guide` component page

--- src/Pages/Guide/index.tsx
+++ src/Pages/Guide/index.tsx
@@ -2,21 +2,26 @@ import { BackButton, InstructionCard } from 'Components'
 import instructionsData from 'Assets/Data/instructionsData.json'
 
 function GuidePage() {
+
+	
+
 	return (
 		<main className='guide-page'>
 			<section className='guide-page__top-section'>
 				<BackButton />
 				<div className='headingXL-container'>
-					<p className='headingXL shadow' aria-hidden='true'>How To Play</p>
+					<p className='headingXL shadow' aria-hidden='true'>
+						How To Play
+					</p>
 					<p className='headingXL'>How To Play</p>
 				</div>
 			</section>
 
-      <section className='guide-page__instructions'>
-        {instructionsData.map((instructionData, index) => (
-          <InstructionCard {...{...instructionData, index}} />
-        ))}
-      </section>
+			<section className='guide-page__instructions'>
+				{instructionsData.map((instructionData, index) => (
+					<InstructionCard {...{ ...instructionData, index }} />
+				))}
+			</section>
 		</main>
 	)
 }
