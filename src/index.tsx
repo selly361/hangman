5d18aef5cc555e1bdc40772a42aa568e76adf7da
selly361 <seikou615@gmail.com>
2024-05-25 18:34:04 +0100

    Update: wrap app with the context providers

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
+import { HangmanProvider, MenuModalProvider } from 'Contexts'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<HangmanProvider>
+				<MenuModalProvider>
+					<App />
+				</MenuModalProvider>
+			</HangmanProvider>
 		</BrowserRouter>
 	</React.StrictMode>
 )
